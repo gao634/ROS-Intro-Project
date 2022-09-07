f7d4063521190558264a5db247a368f2682b5e52
yitian <yitian.ca@gmail.com>
2022-09-07 13:13:42 -0400

    initial commit

---
+++ intro-project/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/gao_yitian/ROS-Intro-Project/intro-project/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
