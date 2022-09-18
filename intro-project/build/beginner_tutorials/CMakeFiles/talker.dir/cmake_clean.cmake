31d6eba4f06dc85fcf24787bdd564567c1d8aeb5
yitian <yitian.ca@gmail.com>
2022-09-17 21:11:50 -0400

    make

---
+++ intro-project/build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/gao_yitian/ROS-Intro-Project/intro-project/devel/lib/beginner_tutorials/talker"
+  "/home/gao_yitian/ROS-Intro-Project/intro-project/devel/lib/beginner_tutorials/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ intro-project/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gao_yitian/ROS-Intro-Project/intro-project/src/beginner_tutorials
+# Build directory: /home/gao_yitian/ROS-Intro-Project/intro-project/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
